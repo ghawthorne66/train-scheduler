b6c2b6a144cf82c5723a0ab31e97945855198bbf
Greg Hawthorne <ghawthorne@me.com>
2019-09-12 09:23:59 -0700

    link firebase and html

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-    // Initialize Firebase
-    var firebaseConfig = {
-        apiKey: "AIzaSyBZxTGt9Y40vDZOCtZROqALsV2EhJAx0AI",
-        authDomain: "cbc-activities.firebaseapp.com",
-        databaseURL: "https://cbc-activities.firebaseio.com",
-        projectId: "cbc-activities",
-        storageBucket: "",
-        messagingSenderId: "155146048251",
-        appId: "1:155146048251:web:18d87ecdcf878262f3565e"
-    };
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBZxTGt9Y40vDZOCtZROqALsV2EhJAx0AI",
+    authDomain: "cbc-activities.firebaseapp.com",
+    databaseURL: "https://cbc-activities.firebaseio.com",
+    projectId: "cbc-activities",
+    storageBucket: "",
+    messagingSenderId: "155146048251",
+    appId: "1:155146048251:web:18d87ecdcf878262f3565e"
+};
 
 
-    firebase.initializeApp(firebaseConfig);
-    // Make sure to match the configuration to the script version number in the HTML
-    // (Ex. 3.0 != 3.7.0)
+firebase.initializeApp(firebaseConfig);
+// Make sure to match the configuration to the script version number in the HTML
+// (Ex. 3.0 != 3.7.0)
 
 
-    // Assign the reference to the database to a variable named 'database'
-    var database = firebase.database();
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
 
 $(document).ready(function () {
 
-// Initial Values
-var trainName
-var destinationPlace
-var frequencyMin
-var nextArrival
-
-$("#add-employee-btn").on("click", function (event) {
-    event.preventDefault();
-    trainName = $("#employee-name-input").val().trim()
-    destinationPlace = $("#role-input").val().trim()
-    frequencyMin = $("#start-input").val().trim()
-    nextArrival = $("#rate-input").val().trim()
-    console.log(employeeName, employeeRole, startDate, monthlyRate)
-
-    //push values to database
-
-    database.ref().push({
-        employeeName: $("#employee-name-input").val().trim(),
-        employeeRole: $("#role-input").val().trim(),
-        startDate: $("#start-input").val().trim(),
-        monthlyRate: $("#rate-input").val().trim()
-
-    }
-
-    )
+    // Initial Values
+    var trainName
+    var destinationPlace
+    var firstTrain
+    var frequencyMin
 
 
-    database.ref().push({
-        name: employeeName,
-        role: employeeRole,
-        start: startDate,
-        rate: monthlyRate,
+    $("#add-scheduler-btn").on("click", function (event) {
+        event.preventDefault();
+        trainName = $("#train-name-input").val().trim()
+        destinationPlace = $("#destination-input").val().trim()
+        firstTrain = $("#time-input").val().trim()
+        frequencyMin = $("#frequency-input").val().trim()
+        console.log(trainName, destinationPlace, firstTrain, frequencyMin)
 
+        //push values to database
+
+        database.ref().push({
+            trainName: $("#train-name-input").val().trim(),
+            destinationPlace: $("#destination-input").val().trim(),
+            firstTrain: $("#time-input").val().trim(),
+            frequencyMin: $("#frequency-input").val().trim()
+
+        }
+
+        )
+
+
+        database.ref().push({
+            train: trainName,
+            destination: destinationPlace,
+            time: firstTrain,
+            frequency: frequencyMin,
+
+        })
     })
-})
-//creating chil push on table-rows
-database.ref().on("child_added", function(snapshot) {
-    $("#table-body").append("<tr></tr>")
+    //creating chil push on table-rows
+    database.ref().on("child_added", function (snapshot) {
+        $("#table-body").append("<tr></tr>")
         // Change the HTML to reflect
         // $("#name-display").text(snapshot.val().name);
         // $("#email-display").text(snapshot.val().email);
@@ -65,8 +66,8 @@ database.ref().on("child_added", function(snapshot) {
         // $("#comment-display").text(
         //   snapshot.val().comment
         console.log(snapshot)
-        
-      
+
+
     });
 
 })
